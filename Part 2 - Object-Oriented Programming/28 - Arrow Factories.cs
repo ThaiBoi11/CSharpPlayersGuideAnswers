f6217feaa28ecc668cdf885cda67af1c4ea15c72
Raghav Prabhakar <119936506+ThaiBoi11@users.noreply.github.com>
2023-04-27 11:11:49 +0100

    Update 28 - Arrow Factories.cs

--- Part 2 - Object-Oriented Programming/28 - Arrow Factories.cs
+++ Part 2 - Object-Oriented Programming/28 - Arrow Factories.cs
@@ -1,41 +1,72 @@
 ArrowheadType userArrowhead = ArrowheadType.steel;
 FletchingType userFletching = FletchingType.plastic;
 int userLength, userChoice;
-Console.WriteLine("What arrowhead would you like? \n1. Steel \n2. Wood \n3. Obsidian");
-userChoice = Convert.ToInt32(Console.ReadLine());
-while (userChoice > 3 || userChoice < 1)
+Console.WriteLine("Would you like a custom arrow? Type \"Yes\" if so");
+string customAnswer = Console.ReadLine();
+if (customAnswer == "Yes")
 {
-    switch (userChoice)
-    {
-        case 1:
-            userArrowhead = ArrowheadType.steel;
-            break;
-        case 2:
-            userArrowhead = ArrowheadType.wood;
-            break;
-        case 3:
-            userArrowhead= ArrowheadType.steel;
-            break;
-        default:
-            Console.WriteLine("Sorry, you have not picked a valid choice. Please try again. ");
-            userChoice = Convert.ToInt32(Console.ReadLine());
-            break;
-    }
-}
-userChoice = 0;
-Console.WriteLine("What fletching would you like? \n1. Plastic \n2. Turkey Feathers \n3. Goose feathers");
-userChoice = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("How long would you like your arrow to be? ");
-userChoice = Convert.ToInt32(Console.ReadLine());
-while (userChoice < 60 || userChoice > 100)
-{
-    Console.WriteLine("Sorry, your arrow is either too short or too long. Make sure your arrow is between 60 and 100 cm. How long would you like your arrow to be? \n");
+    Console.WriteLine("What arrowhead would you like? \n1. Steel \n2. Wood \n3. Obsidian");
     userChoice = Convert.ToInt32(Console.ReadLine());
+    while (userChoice > 3 || userChoice < 1)
+    {
+        switch (userChoice)
+        {
+            case 1:
+                userArrowhead = ArrowheadType.steel;
+                break;
+            case 2:
+                userArrowhead = ArrowheadType.wood;
+                break;
+            case 3:
+                userArrowhead = ArrowheadType.steel;
+                break;
+            default:
+                Console.WriteLine("Sorry, you have not picked a valid choice. Please try again. ");
+                userChoice = Convert.ToInt32(Console.ReadLine());
+                break;
+        }
+    }
+    userChoice = 0;
+    Console.WriteLine("What fletching would you like? \n1. Plastic \n2. Turkey Feathers \n3. Goose feathers");
+    userChoice = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("How long would you like your arrow to be? ");
+    userChoice = Convert.ToInt32(Console.ReadLine());
+    while (userChoice < 60 || userChoice > 100)
+    {
+        Console.WriteLine("Sorry, your arrow is either too short or too long. Make sure your arrow is between 60 and 100 cm. How long would you like your arrow to be? \n");
+        userChoice = Convert.ToInt32(Console.ReadLine());
+    }
+    userLength = userChoice;
+    userChoice = 0;
+    Arrow CustomArrow = new Arrow(userArrowhead, userFletching, userLength);
+    Console.WriteLine($"Your arrow will cost{CustomArrow.GetCost}");
 }
-userLength = userChoice;
-userChoice = 0;
-Arrow CustomArrow = new Arrow(userArrowhead, userFletching, userLength);
-Console.WriteLine($"Your arrow will cost {CustomArrow.GetCost(userArrowhead, userFletching, userLength)} coins.");
+else
+{
+    int presetChoice = -1;
+    Arrow presetArrow = new Arrow(ArrowheadType.steel, FletchingType.plastic, 100);
+    Console.WriteLine("Select an preset arrow. \n1. The Elite Arrow \n2. The Beginner Arrow \n3. The Marksman Arrow.");
+    while (presetChoice < 1 || presetChoice > 3)
+    {
+        presetChoice = int.Parse(Console.ReadLine());
+        switch (presetChoice)
+        {
+            case 1:
+                presetArrow = Arrow.CreateEliteArrow();
+                break;
+            case 2:
+                presetArrow = Arrow.CreateBeginnerArrow();
+                break;
+            case 3:
+                presetArrow = Arrow.CreateMarksmanArrow();
+                break;
+            default:
+                Console.WriteLine("Apologies, you have not input a valid choice. Please try again.");
+                break;
+        }
+    }
+    Console.WriteLine($"Your arrow will cost {presetArrow.GetCost(presetArrow)}");
+}
 class Arrow
 {
     private ArrowheadType _arrowhead;
@@ -47,10 +78,10 @@ class Arrow
         _fletching = fletching;
         _shaft = shaft;
     }
-    public float GetCost(ArrowheadType arrowhead, FletchingType fletching, int shaft)
+    public float GetCost(Arrow arrow)
     {
         float cost = 0;
-        switch (arrowhead)
+        switch (arrow._arrowhead)
         {
             case ArrowheadType.steel:
                 cost += 10;
@@ -64,7 +95,7 @@ class Arrow
             default:
                 break;
         }
-        switch (fletching)
+        switch (arrow._fletching)
         {
             case FletchingType.plastic:
                 cost += 10;
@@ -78,21 +109,24 @@ class Arrow
             default:
                 break;
         }
-        return cost + ((float)(shaft * 0.05));
+        return cost + ((float)(arrow._shaft * 0.05));
 
     }
 
     public int Shaft { get; set; }
     public ArrowheadType Arrowhead { get; set; }
     public FletchingType Fletching { get; set; }
-    public static Arrow CreateEliteArrow() {
-        Arrow EliteArrow = new Arrow(ArrowheadType.steel, FletchingType.plastic, 95);
+    public static Arrow CreateEliteArrow()
+    {
+        return new Arrow(ArrowheadType.steel, FletchingType.plastic, 95);
     }
-    public static Arrow CreateBeginnerArrow() {
-        Arrow BeginnerArrow = new Arrow(ArrowheadType.wood, FletchingType.goosefeathers, 75);
+    public static Arrow CreateBeginnerArrow()
+    {
+        return new Arrow(ArrowheadType.wood, FletchingType.goosefeathers, 75);
     }
-    public static Arrow CreateMarksmanArrow() {
-        Arrow MarksmanArrow = new Arrow(ArrowheadType.steel, FletchingType.goosefeathers, 65);
+    public static Arrow CreateMarksmanArrow()
+    {
+        return new Arrow(ArrowheadType.steel, FletchingType.goosefeathers, 65);
     }
 }
 enum ArrowheadType
